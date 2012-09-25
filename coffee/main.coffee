832aaf6c53fee9e683bed22c634fc43f6f3730ba
Vladimir Kovalskiy <kovalskiy@tncor.com>
2012-09-25 22:48:49 +0400

    added vk.com

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -3,7 +3,20 @@ pageMap =
     id: '7UDFVQ71'
   'ru':
     id: '4F5IJTYE'
+    showVk: yes
 
 config = pageMap[document.location.search?.match(/(..)$/i)?[1]] ? pageMap['en']
 
+# setup launchrock widget
 $('.lrdiscoverwidget').attr('rel', config.id)
+
+setTimeout =>
+  if config.showVk
+    $("#sharebuttons-#{config.id}").append $(\
+    """
+      <span id="vk_like" class="facebook"></span>
+      <script type="text/javascript">
+        VK.Widgets.Like("vk_like", {type: "mini", height: 20});
+      </script>
+    """)
+, 4000
--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 (function() {
-  var config, pageMap, _ref, _ref1, _ref2;
+  var config, pageMap, _ref, _ref1, _ref2,
+    _this = this;
 
   pageMap = {
     'en': {
       id: '7UDFVQ71'
     },
     'ru': {
-      id: '4F5IJTYE'
+      id: '4F5IJTYE',
+      showVk: true
     }
   };
 
   config = (_ref = pageMap[(_ref1 = document.location.search) != null ? (_ref2 = _ref1.match(/(..)$/i)) != null ? _ref2[1] : void 0 : void 0]) != null ? _ref : pageMap['en'];
 
   $('.lrdiscoverwidget').attr('rel', config.id);
+
+  setTimeout(function() {
+    if (config.showVk) {
+      return $("#sharebuttons-" + config.id).append($("<span id=\"vk_like\" class=\"facebook\"></span>\n<script type=\"text/javascript\">\n  VK.Widgets.Like(\"vk_like\", {type: \"mini\", height: 20});\n</script>"));
+    }
+  }, 4000);
 
 }).call(this);
