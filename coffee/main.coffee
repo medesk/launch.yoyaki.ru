8b8be93505247dd233a399e8fb5e2ea7621554f8
Vladimir Kovalskiy <kovalskiy@tncor.com>
2012-09-26 00:59:39 +0400

    added custom url sharing for vk.com

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -11,27 +11,41 @@ config = pageMap[document.location.search?.match(/(..)$/i)?[1]] ? pageMap['en']
 # setup launchrock widget
 $('.lrdiscoverwidget').attr('rel', config.id)
 
-setTimeout =>
-  if config.showVk
-    $("#sharebuttons-#{config.id}").append $(\
-    """
-      <script type="text/javascript">
-      </script>
-      <span id="vk_like" style="display:inline-block"></span>
-      <script type="text/javascript">
-        VK.init({apiId: 3143951, onlyWidgets: true});
-        VK.Widgets.Like("vk_like", {type: "mini", height: 20, width: 76});
-        var vkFix = function() {
-          $('#vk_like').css('width','76px');
+shareText = null
+
+monitorTextChange = =>
+  shareText = $("#shareurl-#{config.id}") unless shareText
+  
+  url = shareText.val()
+  unless url.match(/yoyaki/)
+    setTimeout monitorTextChange, 1000
+  else
+    if config.showVk
+      $("#sharebuttons-#{config.id}").append $(\
+      """
+        <script type="text/javascript">
+        </script>
+        <span id="vk_like" style="display:inline-block"></span>
+        <script type="text/javascript">
+          VK.init({apiId: 3143951, onlyWidgets: true});
+          VK.Widgets.Like("vk_like", {
+            type: "mini", 
+            height: 20,
+            width: 76, 
+            pageUrl: '#{url}',
+            pageDescription: 'Тестовое описание'});
+          var vkFix = function() {
+            $('#vk_like').css('width','76px');
+            setTimeout(vkFix, 1000);
+          }
           setTimeout(vkFix, 1000);
-        }
-        setTimeout(vkFix, 1000);
-      </script>
-    """)
-  if config.showOk
-    $("#sharebuttons-#{config.id}").append $(\
-    """
-    <a target="_blank" class="mrc__plugin_uber_like_button" href="http://connect.mail.ru/share" data-mrc-config="{'nc' : '1', 'cm' : '1', 'ck' : '1', 'sz' : '20', 'st' : '2', 'tp' : 'ok', 'width': 76}">Нравится</a>
-    <script src="http://cdn.connect.mail.ru/js/loader.js" type="text/javascript" charset="UTF-8"></script>
-    """)
-, 4000
+        </script>
+      """)
+    if config.showOk
+      $("#sharebuttons-#{config.id}").append $(\
+      """
+      <a target="_blank" class="mrc__plugin_uber_like_button" href="http://connect.mail.ru/share" data-mrc-config="{'nc' : '1', 'cm' : '1', 'ck' : '1', 'sz' : '20', 'st' : '2', 'tp' : 'ok', 'width': 76}">Нравится</a>
+      <script src="http://cdn.connect.mail.ru/js/loader.js" type="text/javascript" charset="UTF-8"></script>
+      """)
+
+setTimeout monitorTextChange, 4000
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function() {
-  var config, pageMap, _ref, _ref1, _ref2,
+  var config, monitorTextChange, pageMap, shareText, _ref, _ref1, _ref2,
     _this = this;
 
   pageMap = {
@@ -17,13 +17,26 @@
 
   $('.lrdiscoverwidget').attr('rel', config.id);
 
-  setTimeout(function() {
-    if (config.showVk) {
-      $("#sharebuttons-" + config.id).append($("<script type=\"text/javascript\">\n</script>\n<span id=\"vk_like\" style=\"display:inline-block\"></span>\n<script type=\"text/javascript\">\n  VK.init({apiId: 3143951, onlyWidgets: true});\n  VK.Widgets.Like(\"vk_like\", {type: \"mini\", height: 20, width: 76});\n  var vkFix = function() {\n    $('#vk_like').css('width','76px');\n    setTimeout(vkFix, 1000);\n  }\n  setTimeout(vkFix, 1000);\n</script>"));
+  shareText = null;
+
+  monitorTextChange = function() {
+    var url;
+    if (!shareText) {
+      shareText = $("#shareurl-" + config.id);
     }
-    if (config.showOk) {
-      return $("#sharebuttons-" + config.id).append($("<a target=\"_blank\" class=\"mrc__plugin_uber_like_button\" href=\"http://connect.mail.ru/share\" data-mrc-config=\"{'nc' : '1', 'cm' : '1', 'ck' : '1', 'sz' : '20', 'st' : '2', 'tp' : 'ok', 'width': 76}\">Нравится</a>\n<script src=\"http://cdn.connect.mail.ru/js/loader.js\" type=\"text/javascript\" charset=\"UTF-8\"></script>"));
+    url = shareText.val();
+    if (!url.match(/yoyaki/)) {
+      return setTimeout(monitorTextChange, 1000);
+    } else {
+      if (config.showVk) {
+        $("#sharebuttons-" + config.id).append($("<script type=\"text/javascript\">\n</script>\n<span id=\"vk_like\" style=\"display:inline-block\"></span>\n<script type=\"text/javascript\">\n  VK.init({apiId: 3143951, onlyWidgets: true});\n  VK.Widgets.Like(\"vk_like\", {\n    type: \"mini\", \n    height: 20,\n    width: 76, \n    pageUrl: '" + url + "',\n    pageDescription: 'Тестовое описание'});\n  var vkFix = function() {\n    $('#vk_like').css('width','76px');\n    setTimeout(vkFix, 1000);\n  }\n  setTimeout(vkFix, 1000);\n</script>"));
+      }
+      if (config.showOk) {
+        return $("#sharebuttons-" + config.id).append($("<a target=\"_blank\" class=\"mrc__plugin_uber_like_button\" href=\"http://connect.mail.ru/share\" data-mrc-config=\"{'nc' : '1', 'cm' : '1', 'ck' : '1', 'sz' : '20', 'st' : '2', 'tp' : 'ok', 'width': 76}\">Нравится</a>\n<script src=\"http://cdn.connect.mail.ru/js/loader.js\" type=\"text/javascript\" charset=\"UTF-8\"></script>"));
+      }
     }
-  }, 4000);
+  };
+
+  setTimeout(monitorTextChange, 4000);
 
 }).call(this);
